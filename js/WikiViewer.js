89d64b327d1c2cb1a8cdc6e1c8ef0eb90a188773
Atul Sharma <atulsharma3108@gmail.com>
2017-05-02 16:51:03 +0900

    Update WikiViewer.js
    Improved Code

--- js/WikiViewer.js
+++ js/WikiViewer.js
@@ -1,52 +1,63 @@
-$(document).ready(function() {
-  $("#searchIcon").click(function() {
+(function(window, document, $, undefined) {
 
-    $("#searchIcon").addClass("hideItem");
+	window.wikiViewer = {};
 
-    $("#queryDiv").removeClass("hideItem");
+	wikiViewer.searchIconClick = function() {
+		console.log("called");
+		$("#searchIcon").addClass("hideItem");
 
-    $("#searchInput").removeClass("shrinkItem");
-    $("#searchInput").addClass("expandItem");
+		$("#queryDiv").removeClass("hideItem");
 
-  });
+		$("#searchInput").removeClass("shrinkItem");
+		$("#searchInput").addClass("expandItem");
+	}
 
-  $("#closeSearch").click(function() {
+	wikiViewer.closeSearchClick = function() {
+		$("#searchInput").removeClass("expandItem");
+		var $searchInput = wikiViewer.reset($("#searchInput"));
+		$searchInput.addClass("shrinkItem");
+		
+		setTimeout(function() {
+			$("#searchIcon").removeClass("hideItem");
+			$("#searchedContent").addClass("hideItem");
+			$("#queryDiv").addClass("hideItem");
+		}, 250);
+		$("#subContainer").addClass('vertical-center-row');
+	}
 
-    $("#searchInput").removeClass("expandItem");
-    var $searchInput = reset($("#searchInput"));
-    $searchInput.addClass("shrinkItem");
-    setTimeout(function() {
-      $("#searchIcon").removeClass("hideItem");
-      $("#searchedContent").addClass("hideItem");
-      $("#queryDiv").addClass("hideItem");
-    }, 250);
-    $("#subContainer").addClass('vertical-center-row');
+	wikiViewer.searchInputKeyPress = function() {
+		var keycode = (event.keyCode ? event.keyCode : event.which);
+		if (keycode == '13') {
 
-  });
+			var endpoint = "https://en.wikipedia.org/w/api.php?action=query&gsrnamespace=0&format=json&prop=extracts&exintro=&exsentences=1&gsrlimit=15&generator=search&origin=*&plimit=max&exlimit=max&gsrsearch=";
 
-  $('#searchInput').keypress(function(event) {
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if (keycode == '13') {
+			var title = $('#searchInput').val();
+			var extraParam = "&format=jsonp&origin=*";
+			$.getJSON(endpoint + title, function(json) {
 
-      var endpoint = "https://en.wikipedia.org/w/api.php?action=query&gsrnamespace=0&format=json&prop=extracts&exintro=&exsentences=1&gsrlimit=15&generator=search&origin=*&plimit=max&exlimit=max&gsrsearch=";
+				$.each(json.query.pages, function(key, value) {
+					console.log(value);
+					$("#searchedContent").append("<div class='panel panel-default' ><a target='_blank' href='http://en.wikipedia.org/?curid=" + value.pageid + "'><h3 class='panel-title'>" + value.title + "</h3><div class='panel-body'>" + value.extract + "</div></a></div>");
+				});
+				$("#subContainer").removeClass('vertical-center-row');
+			});
+		}
+	}
 
-      var title = $('#searchInput').val();
-      var extraParam = "&format=jsonp&origin=*";
-      $.getJSON(endpoint + title, function(json) {
+	wikiViewer.reset = function($elem) {
+		$elem.before($elem.clone(true,true));
+		var $newElem = $elem.prev();
+		$elem.remove();
+		return $newElem;
+	}
 
-        $.each(json.query.pages, function(key, value) {
-          console.log(value);
-          $("#searchedContent").append("<div class='panel panel-default' ><a target='_blank' href='http://en.wikipedia.org/?curid=" + value.pageid + "'><h3 class='panel-title'>" + value.title + "</h3><div class='panel-body'>" + value.extract + "</div></a></div>");
-        });
-        $("#subContainer").removeClass('vertical-center-row');
-      });
-    }
-  });
-});
+	$(function() {
 
-function reset($elem) {
-  $elem.before($elem.clone(true));
-  var $newElem = $elem.prev();
-  $elem.remove();
-  return $newElem;
-} // end reset()
+		// The DOM is ready!
+		$("#searchIcon").on('click', wikiViewer.searchIconClick);
+		$("#closeSearch").on('click', wikiViewer.closeSearchClick);
+		$("#searchInput").on('keypress', wikiViewer.searchInputKeyPress);
+
+	});
+
+})(window, document, jQuery);
